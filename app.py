2eac98f4375485818e1990a9f9a59df639324482
jeffpace72 <jeff.pace@aezion.com>
2021-02-06 18:41:33 -0600

    ~

--- app.py
+++ app.py
@@ -4,6 +4,32 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
