7c53527056992a16aec157c4f914a4e67d1c5d8a
ADRIAN GROPPER <agropper@healthurl.com>
2021-03-28 20:46:57 -0400

    Move server to app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
-from flask import render_template
+import stripe
+import os
 
-app = Flask(__name__)
+from flask import Flask, render_template, jsonify, request
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+
+app = Flask(__name__, static_folder='../client', static_url_path='', template_folder='../client')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/public-keys')
+def public_keys():
+    return jsonify({ 'key': os.getenv('STRIPE_PUBLISHABLE_KEY') })
+
+
+@app.route('/my-route', methods=['POST'])
+def my_route():
+    print('This is `test`: ')
+    print(request.json['test'])
+    return jsonify({ 'request': request.json })
+
+    
+app.run(port=8080)
---
+++ x-app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
