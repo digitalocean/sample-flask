c0c50a5752c70e48751cdbbfa73cffa25a7b7eff
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2021-03-31 11:01:45 -0300

    Fix for running into DO

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ load_dotenv(find_dotenv())
 
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
 
-app = Flask(__name__, static_folder='../client', static_url_path='', template_folder='../client')
+app = Flask(__name__, static_folder='../client',
+            static_url_path='', template_folder='../client')
+
 
 @app.route('/')
 def index():
@@ -17,14 +19,11 @@ def index():
 
 @app.route('/public-keys')
 def public_keys():
-    return jsonify({ 'key': os.getenv('STRIPE_PUBLISHABLE_KEY') })
+    return jsonify({'key': os.getenv('STRIPE_PUBLISHABLE_KEY')})
 
 
 @app.route('/my-route', methods=['POST'])
 def my_route():
     print('This is `test`: ')
     print(request.json['test'])
-    return jsonify({ 'request': request.json })
-
-    
-app.run(port=8080)
+    return jsonify({'request': request.json})
