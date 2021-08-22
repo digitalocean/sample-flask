0a2eb1c7a2229e6d39f1f6530c57ccca5d07db31
Tim <tjd-dev@users.noreply.github.com>
2021-08-21 21:06:43 -0500

    key fix

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from requests import HTTPError
 
 app = Flask(__name__)
 
-openai.api_key = OPENAI_API_KEY="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"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 start_sequence = "\nAI:"
 restart_sequence = "\nHuman: "
