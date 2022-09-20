544bc886cc4b74e502f9dbf3755290dcef322acd
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-09-20 10:29:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
+import mysql.connector
+
+def connect_db():
+    midb = mysql.connector.connect(
+    host="141.136.39.86",
+    user="mmslogis_GS",
+    password="12345",
+    database="mmslogis_MMSPack"
+    )
+    return midb
+
+def verificar_conexion(midb):
+    if midb.is_connected() == False:
+        print("Reconectando base de datos")
+    while midb.is_connected() == False:
+        try:
+            midb = connect_db()
+            conexion = midb.is_connected()
+            print("Conexion exitosa")
+        except:
+            print("Error en la coneccion")
+    return midb
+
 
 from flask import Flask
 app = Flask(__name__)
@@ -11,11 +34,10 @@ app.config.from_mapping(
 def bienvenido():
     return "Hola Mundo"
 
-from database import database
 @app.route("/api/users/create",methods=["POST","GET"])
 def test():
     data = request.get_json()
-    midb = database.connect_db()
+    midb = connect_db()
     cursor = midb.cursor()
     cursor.execute(f"insert into empleado (nombre,puesto,vehiculo,patente,correo,dni,cbu,telefono,direccion,localidad,password) values('{data['nombre']}','{data['puesto']}','{data['vehiculo']}','{data['patente']}','{data['correo']}','{data['dni']}','{data['cbu']}','{data['telefono']}','{data['direccion']}','{data['localidad']}','{data['password']}')")
     midb.commit()
@@ -26,7 +48,7 @@ def test():
 @app.route("/api/users/login",methods=["POST","GET"])
 def loginAppMovil():
     dataLogin = request.get_json()
-    midb = database.connect_db()
+    midb = connect_db()
     cursor = midb.cursor()
     cursor.execute(f"select id,nombre,password from empleado where dni = {dataLogin['dni']}")
     res = cursor.fetchone()
