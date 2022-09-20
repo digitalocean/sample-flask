087065a558b58ba114a0b455ef5d544f0e393d76
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-09-20 11:47:20 -0300

    actualizaso

--- MeLi/MeLi.py
+++ MeLi/MeLi.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*- 
 # encoding: utf-8
 
-from ftplib import error_proto
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify, make_response
-
+from database import database
 from MeLi.script import consultar_envio
 from informeErrores import informeErrores
 ML = Blueprint('MeLi', __name__, url_prefix='/')
 from datetime import datetime
-import pymysql
 
 secret_key = "abcd1234"
 @ML.route("/callbacks", methods=["GET","POST"])
@@ -51,7 +49,7 @@ def usuario_vinculado():
         code = request.form["code"]
         state = request.form["state"]
         if state == secret_key:
-            midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='viajesbarracas', charset = 'utf8mb4')
+            midb = database.connect_db()
             cursor = midb.cursor()
             cursor.execute("insert into usuario (nickname, contraseña,correo_electronico, tipo_usuario, refresh_token) values(%s,%s,%s,%s,%s)", (nickname, contrasenia,correo_electronico, "Cliente", code))
             midb.commit()
@@ -74,7 +72,7 @@ def recibirnotificacion():
 
     if request.method == "POST":
         nros_envios = []
-        midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='mmspack-almagro', charset = 'utf8mb4')
+        midb = database.connect_db()
         cursor = midb.cursor()
         cursor.execute("select Numero_envío from ViajesFlexs")
         envios = cursor.fetchall()
@@ -90,7 +88,7 @@ def recibirnotificacion():
         file.write("\nRecursos obtenidos\n")
         file.write(str(topic))
         if str(topic) == "shipments":
-            midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='mmspack-almagro', charset = 'utf8mb4')
+            midb = database.connect_db()
             cursor = midb.cursor()
             cursor.execute("insert into registro_ml (resource, user_id, topic, received, attempts, application_id, sent) values(%s,%s,%s,%s,%s,%s,%s)", (resource,user_id,topic,received,attempts,application_id,sent))
             midb.commit()
@@ -108,18 +106,18 @@ def recibirnotificacion():
                 fecha_creacion = viaje[7]
                 nro_venta = viaje[8]
                 direccion_concatenada = direccion + ", " + localidad + ", Buenos aires"
-                midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='mmspack-almagro', charset = 'utf8mb4')
+                midb = database.connect_db()
                 while midb.open == False:
-                    midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='mmspack-almagro', charset = 'utf8mb4')
+                    midb = database.connect_db()
                 cursor = midb.cursor()
                 cursor.execute("insert into ViajesFlexs (Fecha, Numero_envío, Direccion, Referencia, Localidad, tipo_envio, Vendedor, estado_envio, comprador,nro_venta,Direccion_Completa) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (fecha_creacion,nro_envio,direccion,referencia,localidad,tipo_envio,user_id,estado,comprador,nro_venta,direccion_concatenada))
                 midb.commit()
                 midb.close()
                 nros_envios.append(x[0])
             else:
-                midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='mmspack-almagro', charset = 'utf8mb4')
+                midb = database.connect_db()
                 while midb.open == False:
-                    midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='mmspack-almagro', charset = 'utf8mb4')
+                    midb = database.connect_db()
                 cursor = midb.cursor()
                 cursor.execute("select * from ViajesFlexs where Numero_envío = '%s'", (nro_envio,))
                 estado_db = cursor[0]
@@ -128,9 +126,9 @@ def recibirnotificacion():
                 if estado_db == "Entregado" or  estado_db == estado:
                     pass
                 else:
-                    midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='mmspack-almagro', charset = 'utf8mb4')
+                    midb = database.connect_db()
                     while midb.open == False:
-                        midb = pymysql.connect(host='190.228.29.62', user='matyacc', passwd='Agustin_1504', db='mmspack-almagro', charset = 'utf8mb4')
+                        midb = database.connect_db()
                     cursor = midb.cursor()
                     cursor.execute("UPDATE `viajesbarracas`.`ViajesFlexs` SET `estado_envio` = '%s' WHERE (`Numero_envío` = '%s');", (estado,nro_envio))
                     midb.commit()
