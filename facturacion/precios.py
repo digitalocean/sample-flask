62e1b40fc2d21538089a4f219ceeaab659667c2b
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-09-25 12:33:08 -0300

    Nuevo Modulo de cambio de precios

--- facturacion/precios.py
+++ facturacion/precios.py
@@ -1,41 +1,52 @@
 from database import database
+from auth import auth
 from flask import (
     Blueprint, g, render_template, request, session,redirect
 )
 
+
 from .script import consultar_clientes,quitarAcento
 precios = Blueprint('precios', __name__, url_prefix='/')
 
+def obtenerTarifas(db):
+    cursor = db.cursor()
+    cursor.execute("select idTarifa from tarifa group by idTarifa")
+    tarifas = []
+    for x in cursor.fetchall():
+        tarifas.append(x[0])
+    return tarifas
+
+
 @precios.route('/facturacion/verprecio', methods=["GET","POST"])
+@auth.login_required
+@auth.admin_required
 def consultarPrecio():
     midb = database.connect_db()
     cursor = midb.cursor()
     if request.method == "POST":
         tarifa = request.form["tarifa"]
-        cursor.execute(f"select (select nombre from zona where id = precioCosto.id_zona) as Zona,precio,id_zona from precioCosto where id_tarifa = {tarifa} group by Zona")
+        cursor.execute(f"select (select nombre from zona where id = indicePrecio.id_zona) as Zona,(select precio from zonaTarifaPrecio where id_tarifa = {tarifa} and id_zona = indicePrecio.id_zona),indicePrecio.id_tarifa,indicePrecio.id_zona from indicePrecio where id_tarifa = {tarifa} group by Zona")
         zonas = []
         for x in cursor.fetchall():
-            zona = [x[0],x[1]]
+            zona = [x[0],x[1],x[2],x[3]]
             zonas.append(zona)
-        return render_template("facturacion/tarifas.html",viajes=zonas,cant_columnas = 2,tarifa=tarifa)
+            print(zona)
+        tarifas = obtenerTarifas(midb)
+        return render_template("facturacion/tarifas.html",viajes=zonas,cant_columnas = 2,tarifa=tarifa,tarifas=tarifas,auth = session.get("user_auth"))
     else:
-        cursor.execute("select idTarifa from tarifa group by idTarifa")
-        tarifas = []
-        for x in cursor.fetchall():
-            tarifas.append(x[0])
-        return render_template("facturacion/tarifas.html",cant_columnas = 2,tarifas=tarifas)
+        tarifas = obtenerTarifas(midb)
+        return render_template("facturacion/tarifas.html",cant_columnas = 2,tarifas=tarifas,auth = session.get("user_auth"))
 
-@precios.route('facturacion/cambioprecio/<tarifa>/<zona>/<precio>')
+@precios.route('facturacion/cambioprecio/', methods=["POST"])
+@auth.login_required
+@auth.admin_required
 def cambiarprecio():
-    for x in request.form.keys():
-        print(x)
-    # midb = database.connect_db()
-    # cursor = midb.cursor()
-    # cursor = midb.cursor()
-    # cursor.execute(f"select (select localidad from localidad where id = precioCosto.id_localidad) as Localidad,(select nombre from zona where id = precioCosto.id_zona) as Zona ,precio from precioCosto where id_tarifa = {tarifa} and id_zona = {zona}")
-    # zonas = []
-    # for x in cursor.fetchall():
-    #     zona = [x[0],x[1],x[2]]
-    #     zonas.append(zona)
-    # return render_template("facturacion/tarifas.html",viajes=zonas,cant_columnas = 3)
-    return redirect("/")
+    if request.method == "POST":
+        tarifa = request.form["tarifa"]
+        zona = request.form["zona"]
+        nuevoprecio = request.form["nuevoprecio"]
+        midb = database.connect_db()
+        cursor = midb.cursor()
+        cursor.execute(f"update zonaTarifaPrecio set precio = {nuevoprecio} where id_tarifa = {tarifa} and id_zona = {zona}")
+        midb.commit()
+        return render_template("facturacion/tarifas.html",cant_columnas = 2,tarifas=obtenerTarifas(database.connect_db()),auth = session.get("user_auth"))
