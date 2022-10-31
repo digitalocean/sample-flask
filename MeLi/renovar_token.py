c256f7a3463463ea589ac49565ac3b715e819787
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-10-30 21:39:19 -0300

    TEST VINCULACION

--- MeLi/renovar_token.py
+++ MeLi/renovar_token.py
@@ -11,15 +11,15 @@ def actualizar_token(idUser):
     if resultado != None:
         id_user = [resultado[0],resultado[1]]
         user.append(id_user)
-    try:
-        data = {
-            "grant_type":"authorization_code",
-            "client_id":4857198121733101,
-            "client_secret":"rsH5HedyMwFMjRm2aaAb8jFN1McNUW9c",
-            "code": user[1] ,
-            "redirect_uri":"https://whale-app-suwmc.ondigitalocean.app/callbacks"
-        }
-        respuesta_ML = requests.post("https://api.mercadolibre.com/oauth/token", data).json()
+    data = {
+        "grant_type":"authorization_code",
+        "client_id":4857198121733101,
+        "client_secret":"rsH5HedyMwFMjRm2aaAb8jFN1McNUW9c",
+        "code": user[1] ,
+        "redirect_uri":"https://whale-app-suwmc.ondigitalocean.app/callbacks"
+    }
+    respuesta_ML = requests.post("https://api.mercadolibre.com/oauth/token", data).json()
+    if "user_id" in respuesta_ML.keys():
         user_id = respuesta_ML["user_id"]
         access_token = respuesta_ML["access_token"]
         refresh_token = respuesta_ML["refresh_token"]
@@ -27,7 +27,7 @@ def actualizar_token(idUser):
         print(sql)
         cursor.execute(sql)
         midb.commit()
-    except:
+    else:
         data = {
             "grant_type":"refresh_token",
             "client_id":4857198121733101,
