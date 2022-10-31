91560aa73bfa93ab392e57e7a9a2390fe9adcfd8
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-10-31 11:00:49 -0300

    TEST VINCULACION

--- MeLi/script.py
+++ MeLi/script.py
@@ -34,33 +34,35 @@ def consultar_envio(nro_envio,idUser):
     midb = database.connect_db()
     cursor = midb.cursor()
     cursor.execute(f"select access_token from usuario where user_id = {idUser}")
-    for x in cursor:
-        authorization = x[0]
-    midb.close()
-    url = f"https://api.mercadolibre.com/shipments/{nro_envio}"
-    payload = ""
-    headers = {"Authorization": f"Bearer {authorization}"}
-    response = requests.request("GET", url, data=payload, headers=headers)
-    response_json = response.json()
-    if "receiver_address" in response_json.keys():
-        dato_envio=response_json["receiver_address"]
-        comprador = dato_envio["receiver_name"]
-        tipo_envio = response_json["logistic_type"]
-        direccion = dato_envio["address_line"]
-        city = dato_envio["city"]
-        localidad = city["name"]
-        referencia = dato_envio["comment"]
-        fecha_creacion = response_json["date_created"]
-        nro_venta = response_json["order_id"]
-        fecha_creacion = str(fecha_creacion)[0:19]
-        estado = response_json["status"]
-        print(str(nro_envio) + " " + str(direccion) + " " + str(estado + " " + tipo_envio))
-        return nro_envio, tipo_envio, direccion, localidad, referencia, estado, comprador, fecha_creacion,nro_venta
-    else:
-        if actualizar_token(idUser) == True:
-            return consultar_envio(nro_envio,idUser)
+    resultado = cursor.fetchone()
+    if resultado != None:
+        for x in resultado:
+            authorization = x[0]
+        midb.close()
+        url = f"https://api.mercadolibre.com/shipments/{nro_envio}"
+        payload = ""
+        headers = {"Authorization": f"Bearer {authorization}"}
+        response = requests.request("GET", url, data=payload, headers=headers)
+        response_json = response.json()
+        if "receiver_address" in response_json.keys():
+            dato_envio=response_json["receiver_address"]
+            comprador = dato_envio["receiver_name"]
+            tipo_envio = response_json["logistic_type"]
+            direccion = dato_envio["address_line"]
+            city = dato_envio["city"]
+            localidad = city["name"]
+            referencia = dato_envio["comment"]
+            fecha_creacion = response_json["date_created"]
+            nro_venta = response_json["order_id"]
+            fecha_creacion = str(fecha_creacion)[0:19]
+            estado = response_json["status"]
+            print(str(nro_envio) + " " + str(direccion) + " " + str(estado + " " + tipo_envio))
+            return nro_envio, tipo_envio, direccion, localidad, referencia, estado, comprador, fecha_creacion,nro_venta
         else:
-            print(f"Error al actualizar access token")
+            if actualizar_token(idUser) == True:
+                return consultar_envio(nro_envio,idUser)
+            else:
+                print(f"Error al actualizar access token")
     
 
 # def subir_viajes():
