c8240cc0e6c7c86e7e026ed2991093d81e28d2cb
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-11-03 00:38:35 -0300

    linck sueldos flexs

--- empleado/sueldo.py
+++ empleado/sueldo.py
@@ -8,14 +8,12 @@ from database import database
 from scriptGeneral import scriptGeneral
 MS = Blueprint('sueldoChofer', __name__, url_prefix='/')
 
-
-
 @MS.route("/misueldo", methods=["GET","POST"])
 @auth.login_required
 def sueldoChofer():
     hoy = str(datetime.now())[0:10]
     if request.method == "GET":
-        return render_template("sueldoChofer.html",
+        return render_template("empleado/sueldoChofer.html",
                         titulo="Facturacion", 
                         desde=hoy,
                         hasta=hoy,
@@ -28,32 +26,72 @@ def sueldoChofer():
         chofer = session.get("user_id")
         desde = request.form["desde"]
         hasta = request.form["hasta"]
-        columnasConsulta = "Fecha,Numero_envío,Direccion_Completa,Vendedor,estado_envio,motivo_noenvio,Costo"
-        cabezeras = ["Fecha","Numero de envío","Direccion","Vendedor","Estado","Motivo","A cobrar"]
+        
         viajes = []
-        sql = f"SELECT {columnasConsulta} FROM mmslogis_MMSPack.sueldos where Chofer = '{chofer}' and Fecha between '{desde}' and '{hasta}';"
-        cursor.execute(sql)
-        viajeSabado = 0
-        suma = 0
-        cantidad = 0
-        for viajeTupla in cursor.fetchall():
-            cantidad += 1
-            if type(viajeTupla[6]) == float:
-                suma += viajeTupla[6]
-            viaje = list(viajeTupla)
-            if viajeTupla[6] == 0:
-                viajeSabado +=1
-            viajes.append(viaje)
-        return render_template("sueldoChofer.html",
-                                cliente=chofer,
-                                desde=desde,
-                                hasta=hasta,
-                                titulo="Mi sueldo", 
-                                cabezeras = cabezeras,
-                                tipo_facturacion="flex", 
-                                viajes=viajes, 
-                                cantidad = f"Cantidad de visitas: {cantidad}",
-                                total=f"${suma} y {viajeSabado} viajes de sabados", 
-                                clientes = scriptGeneral.consultar_clientes(midb), 
-                                auth = session.get("user_auth")
-                                )
+        if session.get("user_auth") != "Chofer":
+            cabezeras = ["Chofer","Sueldo"]
+            sql = f"""
+            select 
+                Chofer,sum(Costo)
+            from 
+                sueldos 
+            where  
+                Fecha between '{desde}' and '{hasta}' 
+            GROUP BY Chofer;"""
+
+            cursor.execute(sql)
+            sabados = 0
+            sinPrecio = 0
+            total = 0
+            for viajeTupla in cursor.fetchall():
+                viaje = list(viajeTupla)
+                total += viaje[1]
+                if viaje[1] == 0:
+                    sabados +=1
+                if viaje[1] == None:
+                    sinPrecio += 1
+                viajes.append(viaje)
+            return render_template("facturacion/sueldoChofer.html",
+                        desde=desde,
+                        hasta=hasta,
+                        titulo="Sueldos Flexs", 
+                        cabezeras = cabezeras,
+                        tipo_facturacion="flex", 
+                        viajes=viajes,
+                        total = total,
+                        auth = session.get("user_auth")
+                        )
+        else:
+            cabezeras = ["Fecha","Numero de envío","Direccion","Vendedor","Estado","Motivo","A cobrar"]
+            sql = f"""
+                    SELECT 
+                        Fecha,Numero_envío,Direccion_Completa,Vendedor,estado_envio,motivo_noenvio,Costo 
+                    FROM 
+                        mmslogis_MMSPack.sueldos 
+                    where 
+                        Chofer = '{chofer}' and Fecha between '{desde}' and '{hasta}';"""
+            cursor.execute(sql)
+            viajeSabado = 0
+            suma = 0
+            cantidad = 0
+            for viajeTupla in cursor.fetchall():
+                cantidad += 1
+                if type(viajeTupla[6]) == float:
+                    suma += viajeTupla[6]
+                viaje = list(viajeTupla)
+                if viajeTupla[6] == 0:
+                    viajeSabado +=1
+                viajes.append(viaje)
+            return render_template("empleado/sueldoChofer.html",
+                                    cliente=chofer,
+                                    desde=desde,
+                                    hasta=hasta,
+                                    titulo="Mi sueldo", 
+                                    cabezeras = cabezeras,
+                                    tipo_facturacion="flex", 
+                                    viajes=viajes, 
+                                    cantidad = f"Cantidad de visitas: {cantidad}",
+                                    total=f"${suma} y {viajeSabado} viajes de sabados", 
+                                    clientes = scriptGeneral.consultar_clientes(midb), 
+                                    auth = session.get("user_auth")
+                                    )
