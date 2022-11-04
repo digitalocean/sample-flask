ed2ef6a89eddebf4300c4c6e3ae66e769d2ee495
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-11-04 12:58:16 -0300

    arreglo uploadML

--- MeLi/MeLi.py
+++ MeLi/MeLi.py
@@ -17,6 +17,7 @@ def vinculacion():
         return redirect("https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=4857198121733101&redirect_uri=https://whale-app-suwmc.ondigitalocean.app/callbacks")
     elif request.method == "GET":
         data = request.args
+        print(data)
         if "code" in data.keys():
             code = data["code"]
             state = data["state"]
