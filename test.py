bf8c187a93888f9a1fadce140d444c9363ab3474
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-12-04 12:57:34 -0300

    correo automatico a cliente cuando falla vinculacion

--- test.py
+++
@@ -1,5 +0,0 @@
-from scriptGeneral import scriptGeneral
-
-scriptGeneral.enviar_correo(["acciaiomatiassebastian@gmail.com"],"Test html",None,None,"""
-        <a href='https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=4857198121733101&redirect_uri=https://whale-app-suwmc.ondigitalocean.app/callbacks'>enlace</a>
-""")
