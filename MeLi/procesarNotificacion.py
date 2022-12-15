833843a8f14dac3513c9ea0ef7b0d553d8e132f6
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-12-15 14:18:12 -0300

    filtrado sanitarios

--- MeLi/procesarNotificacion.py
+++ MeLi/procesarNotificacion.py
@@ -1,6 +1,6 @@
 from database import database
 from .consultar_envio import consultar_envio
-from .script import traducirEstado,consultaChoferMeli
+from .script import traducirEstado,consultaChoferMeli,agregarEnvio
 import requests
 def procesarNotificacion(data):
     resource = data.get("resource")
@@ -22,29 +22,13 @@ def procesarNotificacion(data):
         if viaje != None:
             tipo_envio= viaje[1]
             if tipo_envio == "self_service": tipo_envio = 2 
-            direccion= viaje[2] 
-            localidad= viaje[3] 
-            referencia= viaje[4]
             estado = traducirEstado(viaje[5])
-            comprador = viaje[6]
-            fecha_creacion = viaje[7]
-            nro_venta = viaje[8]
-            direccion_concatenada = direccion + ", " + localidad + ", Buenos aires"
-            if estado == "En Camino": consultaChoferMeli(nro_envio,user_id);
-            if resEnvio == None and tipo_envio == 2:
-                midb = database.connect_db()
-                cursor = midb.cursor()
-                sql = """insert into ViajesFlexs 
-                            (Fecha, Numero_envío, Direccion, Referencia, Localidad, tipo_envio, Vendedor, estado_envio, comprador,nro_venta,Direccion_Completa) 
-                        values
-                            (%s,%s,%s,%s,%s,2,apodoOcliente(apodo(%s)),%s,%s,%s,%s)"""
-                values = (fecha_creacion,nro_envio,direccion,referencia,localidad,user_id,"Listo para retirar",comprador,nro_venta,direccion_concatenada)
-                cursor.execute(sql,values)
-                midb.commit()
-                print(f"Envio: {nro_envio} Agregado")
+            if resEnvio == None:
+                if tipo_envio == 2: agregarEnvio(viaje,nro_envio,user_id)
             else:
+                if estado == "En Camino": consultaChoferMeli(nro_envio,user_id)
                 estadoDb = resEnvio[1]
-                if tipo_envio == 2 and estadoDb != estado and estado in ("Cancelado","Listo para retirar"):
+                if tipo_envio == 2 and estadoDb != estado and estado == "Cancelado":
                     midb = database.connect_db()
                     cursor = midb.cursor()
                     sqlCancelado = f"update ViajesFlexs set estado_envio = '{estado}', Motivo = 'Venta cancelada' where Numero_envío = '{nro_envio}'"
