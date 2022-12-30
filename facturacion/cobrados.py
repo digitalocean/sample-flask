cbf7331c0edf2af2786a53189bee784d4a130d19
Matyacc <89623391+Matyacc@users.noreply.github.com>
2022-12-29 22:30:14 -0300

    actualizo

--- facturacion/cobrados.py
+++ facturacion/cobrados.py
@@ -20,7 +20,7 @@ def busqueda():
     mjstbla = ""
     midb = database.connect_db()
     cursor = midb.cursor()
-    sql = f"select Fecha,Zona,Numero_envío,Direccion,Referencia,Localidad,Vendedor,Chofer,estado_envio,Motivo,Reprogramaciones from ViajesFlexs where not Cobrar > 0 or Numero_envío like '%cobrar%' or Referencia like '%cobrar%' order by Fecha desc;"
+    sql = f"select Fecha,Zona,Numero_envío,Direccion,Referencia,Localidad,Vendedor,Chofer,estado_envio,Motivo,Reprogramaciones from ViajesFlexs where Cobrar > 0 or Numero_envío like '%cobrar%' or Referencia like '%cobrar%' order by Fecha desc;"
     cabezeras = ["Fecha","Zona","Numero_envío","Direccion","Referencia","Localidad","Vendedor","Chofer","estado_envio","Motivo","Reprogramaciones"]
     cursor.execute(sql)
     lista = []
