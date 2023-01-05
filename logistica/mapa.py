40e1c86ff10deef7ebe5e82b17268ccae1b7e082
Matyacc <89623391+Matyacc@users.noreply.github.com>
2023-01-05 12:41:25 -0300

    actualizo

--- logistica/mapa.py
+++ logistica/mapa.py
@@ -75,7 +75,7 @@ def cambioZona():
         envio = request.form.get("envio")
         midb = database.connect_db()
         cursor = midb.cursor()
-        sql = f"update ViajesFlexs set Zona = concat(concat({zona},'/'),tipo_envio) where Numero_envío = '{envio}'"
+        sql = f"update ViajesFlexs set Zona = concat({zona},'/'),tipo_envio) where Numero_envío = '{envio}'"
         cursor.execute(sql)
         midb.commit()
         print(sql)
