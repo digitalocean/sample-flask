3ea4fe1ea723808faf3a88d1832da9b9bf69d18e
Matyacc <89623391+Matyacc@users.noreply.github.com>
2023-01-26 14:57:43 -0300

    clientes

--- usuarios/usuarios.py
+++ usuarios/usuarios.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, session
+    Blueprint, render_template, request, session,redirect
 )
 from auth import auth
 from datetime import datetime
@@ -29,8 +29,7 @@ def crear_cliente():
             tarifas.append([id,tar])
         return render_template("usuario/nuevo_cliente.html",
                                 auth = session.get("user_auth"),
-                                tarifas=tarifas)
-
+                                tarifas=tarifas,urlForm = "/clientes/nuevo_cliente")
     elif(request.method == "POST"):
         nombre = request.form.get("nombre")
         razon_social = request.form.get("razon_social")
@@ -62,7 +61,7 @@ def crear_cliente():
             modo_cobro,correo_electronico,tarifa))
         midb.commit()
         midb.close()
-        return render_template("usuario/nuevo_cliente.html",titulo="Nuevo Cliente", auth = session.get("user_auth"))
+        return render_template("usuario/nuevo_cliente.html",titulo="Nuevo Cliente", auth = session.get("user_auth"),urlForm = "/clientes/nuevo_cliente")
 
 @us.route("clientes")
 @auth.login_required
@@ -85,35 +84,67 @@ def modificarDatosClientes():
     midb = database.connect_db()
     cursor = midb.cursor()
     if request.method == "GET":
-        idCliente = request.form.get("idCliente")
-        sql = """ select `nombre_cliente`,`razon_social`,`CUIT`,`CBU`,`Direccion`,`Latlong`,`Telefono`,
-                `Telefono_Alternativo`,`password`,`Fecha_Baja`,`Modalidad_cobro`,`correo_electronico`,
+        cursor.execute("select id, nombre from tarifa")
+        tarifas = []
+        for x in cursor.fetchall():
+            id = x[0]
+            tar = x[1]
+            tarifas.append([id,tar])
+        idClientes = request.args.get("idClientes")
+        sql = """ select `nombre_cliente`,`razon_social`,`CUIT`,`Direccion`,`Telefono`,
+                `Telefono_Alternativo`,`password`,`Modalidad_cobro`,`correo_electronico`,
                 `tarifa` from Clientes where idClientes = %s"""
-        values = (idCliente,)
+        values = (idClientes,)
         cursor.execute(sql,values)
         resu = cursor.fetchone()
-        print(resu)
-        return render_template("usuario/formularioEdicionCliente.html")
+        nombre_cliente = resu[0]
+        razon_social = resu[1]
+        cuit = resu[2]
+        direccion = resu[3]
+        telefono = resu[4]
+        telefonoAlternativo = resu[5]
+        password = resu[5]
+        modalidadCobro = resu[7]
+        correo = resu[8]
+        tarifa = resu[9]
+        midb.close()
+        return render_template("usuario/nuevo_cliente.html",nombre_cliente = nombre_cliente,
+                                razon_social = razon_social,cuit = cuit,direccion = direccion,
+                                telefono = telefono,telefonoAlternativo = telefonoAlternativo,
+                                password = password,modalidadCobro = modalidadCobro,correo = correo,
+                                tarifa = tarifa,tarifas=tarifas,idClientes=idClientes, auth = session.get("user_auth"),
+                                urlForm = "/clientes/modificar")
     else:
+        idClientes = request.form.get("idClientes")
+        nombre_cliente = request.form.get("nombre")
+        razon_social = request.form.get("razon_social")
+        cuit = request.form.get("cuit")
+        direccion = request.form.get("direccion")
+        telefono = request.form.get("telefono")
+        telefonoAlternativo = request.form.get("telefonoAlternativo")
+        password = request.form.get("password")
+        modalidadCobro = request.form.get("modCobro")
+        correo = request.form.get("correo_electronico")
+        tarifa = request.form.get("tarifa")
         sql = """
             UPDATE `mmslogis_MMSPack`.`Clientes`
                 SET
                 `nombre_cliente` = %s,
                 `razon_social` = %s,
                 `CUIT` = %s,
-                `CBU` = %s,
                 `Direccion` = %s,
-                `Latlong` = %s,
                 `Telefono` = %s,
                 `Telefono_Alternativo` = %s,
                 `password` = %s,
-                `Fecha_Baja` = %s,
                 `Modalidad_cobro` = %s,
                 `correo_electronico` = %s,
                 `tarifa` = %s
-                WHERE `idClientes` = %s AND `nombre_cliente` = %s;
+                WHERE `idClientes` = %s;
             """
-        values = (nombre_cliente,razon_social,cuit,cbu,direccion,latlong,telefono,telefonoAlternativo,
-                password,fechaBaja,modalidadCobro,correo,tarifa)
-
-
+        values = (nombre_cliente,razon_social,cuit,direccion,telefono,telefonoAlternativo,
+                password,modalidadCobro,correo,tarifa,idClientes)
+        print(values)
+        cursor.execute(sql,values)
+        midb.commit()
+        midb.close()
+        return redirect("/clientes")
