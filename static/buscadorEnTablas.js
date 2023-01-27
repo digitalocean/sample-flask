4e568b85168d43ffae8023dff74a4b9cff490a15
Matyacc <89623391+Matyacc@users.noreply.github.com>
2023-01-27 10:04:44 -0300

    buscadores

---
+++ static/buscadorEnTablas.js
@@ -0,0 +1,25 @@
+function searchTable() {
+      // Obtener el valor de búsqueda ingresado por el usuario
+      var input = document.getElementById("searchInput").value;
+      // Obtener todas las filas de la tabla
+      var rows = document.getElementById("myTable").getElementsByTagName("tr");
+
+      // Recorrer cada fila
+      for (var i = 0; i < rows.length; i++) {
+        // Obtener todas las celdas de la fila actual
+        var cells = rows[i].getElementsByTagName("td");
+        var found = false;
+        // Recorrer cada celda
+        for (var j = 0; j < cells.length; j++) {
+          // Comparar el valor de la celda con el valor de búsqueda
+          if (cells[j].innerHTML.toLowerCase().indexOf(input.toLowerCase()) > -1) {
+            found = true;
+          }
+        }
+        if (found) {
+          rows[i].style.display = "";
+        } else {
+          rows[i].style.display = "none";
+        }
+      }
+    }
