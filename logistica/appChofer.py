4c582332b9808c484cdff43417a64e758d835395
Matyacc <89623391+Matyacc@users.noreply.github.com>
2023-02-05 00:13:12 -0300

    v2.0

--- logistica/appChofer.py
+++ logistica/appChofer.py
@@ -31,7 +31,7 @@ def loginEmpleado():
 
 @pd.route("/store/apps/MMSPACK-Reparto")
 def descargaAppReparto():
-    return send_file("static/debug/RepartoMMS.apk", as_attachment=True)
+    return send_file("static/debug/RepartoMMS1.0.apk", as_attachment=True)
 @pd.route("/api/user/appversion",methods=["POST"])
 def appVersion():
     return "0.02"
