ad8cf058c0d007989696491213fbb4c91c36a815
Matyacc <89623391+Matyacc@users.noreply.github.com>
2023-02-09 09:45:23 -0300

    mapa chip solo fecha de hoy

--- logistica/mapa.py
+++ logistica/mapa.py
@@ -14,6 +14,8 @@ consultaMapa = """
         and 
             estado_envio in ('Lista Para Retirar','Para preparar','Retirado','Listo para salir (Sectorizado)') 
         and
+            not (tipo_envio = 15 and fecha < current_date())
+        and
             not (estado_envio = "Lista Para Retirar" and Fecha > current_date())
         """
 
