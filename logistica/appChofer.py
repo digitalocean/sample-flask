74e84f3ae6dd1ab27caa2b946139e81bbfd183f1
Matyacc <89623391+Matyacc@users.noreply.github.com>
2023-03-04 11:39:29 -0300

    act

--- logistica/appChofer.py
+++ logistica/appChofer.py
@@ -270,20 +270,14 @@ def entregado():
     print(data)
     nroEnvio = data["nEnvio"]
     chofer = data["chofer"]
-    motivo = None
-    if "motivo" in data.keys():
-        motivo = data["motivo"]
-    observacion,recibe,dni,quienRecibe,ubicacion = None,None,None,None,None
-    if "observacion" in data.keys():
-        observacion = data["observacion"]
+    location = data["location"]
+    motivo = data["motivo"]
+    observacion = data["observacion"]
+    recibe,dni,quienRecibe= None,None,None
     if "quienRecibe" in data.keys() and "dni" in data.keys():
         recibe = data["quienRecibe"] 
         dni = data["dni"]
         quienRecibe = f"{recibe} Dni: {dni}"
-    if "location" in data.keys():
-        location = data["location"]
-    else:
-        location = None
     foto = None
     if "image" in data.keys():
             imagen = data["image"]
