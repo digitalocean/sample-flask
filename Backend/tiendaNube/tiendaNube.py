150bfcf89e8a3ec1d30a43b8b7811a60fcf165db
Matyacc <89623391+Matyacc@users.noreply.github.com>
2023-03-12 10:31:02 -0300

    act

--- Backend/tiendaNube/tiendaNube.py
+++ Backend/tiendaNube/tiendaNube.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template,redirect, request, session
 import requests
+import json
 import os
 from Backend.database.database import connect_db
 TN = Blueprint('TN', __name__, url_prefix='/')
@@ -19,10 +20,11 @@ def vinvulacionTiendaNube():
     }
 
     response = requests.post("https://www.tiendanube.com/apps/authorize/token", data=payload)
-    access_token = response["access_token"]
-    token_type = response["token_type"]
-    scope = response["scope"]
-    user_id = response["user_id"]
+    json_data = json.loads(response.text)
+    access_token = json_data["access_token"]
+    token_type = json_data["token_type"]
+    scope = json_data["scope"]
+    user_id = json_data["user_id"]
     midb = connect_db()
     cursor = midb.cursor()
     cursor.execute("""
