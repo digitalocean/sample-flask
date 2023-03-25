4556daeefeab9cbc7785fe7314db8ee215df6df3
Matyacc <89623391+Matyacc@users.noreply.github.com>
2023-03-25 13:29:16 -0300

    ACT ftp environments

--- Backend/ftp/FTP.py
+++ Backend/ftp/FTP.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,send_file,request
 from io import BytesIO
 import uuid
-import mimetypes
+import os
 
 
 from ftplib import FTP
@@ -17,8 +17,11 @@ def generate_unique_filename(filename):
 def upload(patch,file,filename):
     # unique_filename = str(uuid.uuid4()) + ".pdf"
     unique_filename = generate_unique_filename(filename)
-    ftp = FTP('109.106.251.113')
-    ftp.login(user='appChofer@mmslogistica.com', passwd='(15042020)_')
+    ftp_server = os.environ.get("FTP_SERVER")
+    ftp_user = os.environ.get("FTP_USER")
+    ftp_password = os.environ.get("FTP_PASSWORD") 
+    ftp = FTP(ftp_server )
+    ftp.login(user=ftp_user, passwd=ftp_password)
     ftp.cwd(patch) # directorio de destino en el servidor FTP
     with BytesIO(file) as f:
         ftp.storbinary('STOR ' + unique_filename, f)
