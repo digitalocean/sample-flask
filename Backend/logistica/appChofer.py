ae92265487fe5f1e90e45027c4963b71e342c955
unknown <mmspackcheck@gmail.com>
2023-04-10 16:55:14 -0300

    cancelar

--- Backend/logistica/appChofer.py
+++ Backend/logistica/appChofer.py
@@ -11,30 +11,25 @@ from datetime import datetime,timedelta
 pd = Blueprint('pendientes', __name__, url_prefix='/')
 
 def actualizar_estado_logixs(mensajero_id, tipo_operacion, path, contenido, id_ml, recibe_dni="1234567", recibe_nombre="titular"):
-    print(str(contenido))
-    midb = connect_db()
-    cursor = midb.cursor()
-    cursor.execute(f"select V.Vendedor,ifnull(R.scanner,ifnull(S.scanner,ifnull(EC.scanner,V.Scanner))) as scanner from ViajesFlexs as V left join retirado as R on V.Numero_envío = R.Numero_envío left join sectorizado as S on V.Numero_envío = S.Numero_envío left join en_camino as EC on V.Numero_envío = EC.Numero_envío where V.Numero_envío = '{id_ml}' limit 1")
-    resultado = cursor.fetchone()
-    print(resultado)
+    nickname = ""
+    resultado = None
     if "sender_id" in contenido.keys():
         sender_id = contenido["sender_id"]
-    else:
-        if resultado != None:
-            sender_id = json.loads(str(resultado[1]).replace("'",'"'))["sender_id"]
-        else:
-            print("No se Obtuvo el sender id")
-
-    if resultado != None:
-        nickname = resultado[0].title()
-        
-    else:
         info = set(requests.get("https://api.mercadolibre.com/users/"+str(sender_id)))
-        nickname = ""
         for infoML in info:
             if "nickname" in str(infoML):
                 nickname = (str(infoML).split(",")[1]).split(":")[1]
                 nickname = nickname.replace('"','')
+    else:
+        midb = connect_db()
+        cursor = midb.cursor()
+        cursor.execute(f"select V.Vendedor,ifnull(R.scanner,ifnull(S.scanner,ifnull(EC.scanner,V.Scanner))) as scanner from ViajesFlexs as V left join retirado as R on V.Numero_envío = R.Numero_envío left join sectorizado as S on V.Numero_envío = S.Numero_envío left join en_camino as EC on V.Numero_envío = EC.Numero_envío where V.Numero_envío = '{id_ml}' limit 1")
+        resultado = cursor.fetchone()
+        if resultado != None:
+            sender_id = json.loads(str(resultado[1]).replace("'",'"'))["sender_id"]
+            nickname = resultado[0].title()
+        else:
+            print("No se obtuvo sender_id")
     url = f"https://www.logixs.com.ar/{path}/envioflex/RecibirScanQR"
     data = {
         "MensajeroId": mensajero_id,
@@ -47,7 +42,6 @@ def actualizar_estado_logixs(mensajero_id, tipo_operacion, path, contenido, id_m
         "recibeDNI": recibe_dni,
         "RecibeNombre": recibe_nombre
     }
-    print(data)
     response = requests.post(url, data=data)
     if response.status_code == 200:
         print(response.content)
