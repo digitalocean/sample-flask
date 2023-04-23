78128578b9f2aa8648b2e26cad2553a3eb68248b
Josue David González <josudavidg@gmail.com>
2023-04-23 10:39:47 -0300

    hola

---
+++ static/assets/js/foodhut.js
@@ -0,0 +1,48 @@
+/*!
+=========================================================
+* FoodHut Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+new WOW().init();
+
+function initMap() {
+    var uluru = {lat: 37.227837, lng: -95.700513};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 8,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+ }
+
+ 
