8f5c2223fb9d36c915c6f97c669f5d04a76c78c4
Rahul Prabhu <98982995+Ruprabhu25@users.noreply.github.com>
2023-10-09 16:34:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,27 +8,27 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/contact")
+@app.route("/contact.html")
 def contact():
     return render_template("contact.html")
 
-@app.route("/gift")
+@app.route("/gift.html")
 def gift():
     return render_template("gift.html")
 
-@app.route("/news")
+@app.route("/news.html")
 def news():
     return render_template("news.html")
 
-@app.route("/photos")
+@app.route("/photos.html")
 def photos():
     return render_template("photos.html")
 
-@app.route("/trade")
+@app.route("/trade.html")
 def trade():
     return render_template("trade.html")
 
-@app.route("/videos")
+@app.route("/videos.html")
 def videos():
     return render_template("videos.html")
 
