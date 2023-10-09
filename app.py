2d7112b5ab4defad75a9eb6f0b3433da4d6e660f
Cliff Wulfman <cwulfman@princeton.edu>
2023-10-09 12:02:29 -0400

    starting to make app.py like ezra

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
+import os
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        )
 
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/", methods=['GET', 'POST'])
+    def index():
+        return render_template("index.html")
+
+    return app
+
