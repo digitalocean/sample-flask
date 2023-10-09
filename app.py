e234d764ed408b67220844bcfd7155fb474e8518
Rahul Prabhu <98982995+Ruprabhu25@users.noreply.github.com>
2023-10-08 22:12:55 -0700

    testing

--- app.py
+++ app.py
@@ -5,5 +5,33 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template("index.html")
+
+@app.route("/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/")
+def gift():
+    return render_template("gift.html")
+
+@app.route("/")
+def news():
+    return render_template("news.html")
+
+@app.route("/")
+def photos():
+    return render_template("photos.html")
+
+@app.route("/")
+def trade():
+    return render_template("trade.html")
+
+@app.route("/")
+def videos():
+    return render_template("videos.html")
+
+
+
+
