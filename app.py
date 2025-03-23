9a025627236a3770b99cbbdd1d9e0efb49b89141
GunBoat Diplomat <gunboat_diplomat1@protonmail.com>
2025-03-23 19:11:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/tempate-creator")
+def tempate-creator():
+    return render_template("gen.html")
